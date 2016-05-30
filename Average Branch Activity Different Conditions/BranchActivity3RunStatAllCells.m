c665389edacd69ddeea8a1bb7dc748ea24881bf8
ChiaraBA <chiara.baragli.11@ucl.ac.uk>
2016-05-30 11:15:28 +0100

    add Bonnie cell6

--- Average Branch Activity Different Conditions/BranchActivity3RunStatAllCells.m
+++ Average Branch Activity Different Conditions/BranchActivity3RunStatAllCells.m
@@ -1,64 +1,65 @@
 
-% % L2 neurons, gratings
-% filesL2{1} = 'C:\Data Analysis\Bonnie\09 February 2016\Cell 1 Run Stat\BranchActivityRunStat 17-Feb-2016.mat';
-% filesL2{2} = 'C:\Data Analysis\Bonnie\09 February 2016\Cell 2 Run Stat\BranchActivityRunStat 17-Feb-2016.mat';
-% filesL2{3} = 'C:\Data Analysis\Bonnie\09 February 2016\Cell 5 Run Stat\BranchActivityRunStat 17-Feb-2016.mat';
-% filesL2{4} = 'C:\Data Analysis\Tina\03 February 2015\Run Stat Cell 1\BranchActivityRunStat 17-Feb-2016.mat';
-% filesL2{5} = 'C:\Data Analysis\Tina\03 February 2015\Run Stat Cell 2\BranchActivityRunStat 17-Feb-2016.mat';
-% filesL2{6} = 'C:\Data Analysis\Mary\09 April 2015\Run Stat\BranchActivityRunStat 17-Feb-2016.mat';
-% filesL2{7} = 'C:\Data Analysis\Veronica\27 November 2014\Cell 2 Run Stat Gratings\BranchActivityRunStat 17-Feb-2016.mat';
-% filesL2{8} = 'C:\Data Analysis\Robinson\26 June 2015\Cell 2 Run Stat\BranchActivityRunStat 11-May-2016.mat';
-% filesL2{9} = 'C:\Data Analysis\Karina\09 February 2016\Cell 5 Run Stat\BranchActivityRunStat 17-Feb-2016.mat';
-% filesL2{10} = 'C:\Data Analysis\Clyde\15 March 2016\Cell 1\BranchActivityRunStat 28-Apr-2016.mat';
-% filesL2{11} = 'C:\Data Analysis\Clyde\15 March 2016\Cell 2\BranchActivityRunStat 28-Apr-2016.mat';
-% filesL2{12} = 'C:\Data Analysis\Bonnie\14 March 2016\BranchActivityRunStat 29-Apr-2016.mat';
+% L2 neurons, gratings
+filesL2{1} = 'C:\Data Analysis\Bonnie\09 February 2016\Cell 1 Run Stat\BranchActivityRunStat 30-May-2016.mat';
+filesL2{2} = 'C:\Data Analysis\Bonnie\09 February 2016\Cell 2 Run Stat\BranchActivityRunStat 17-Feb-2016.mat';
+filesL2{3} = 'C:\Data Analysis\Bonnie\09 February 2016\Cell 5 Run Stat\BranchActivityRunStat 17-Feb-2016.mat';
+filesL2{4} = 'C:\Data Analysis\Tina\03 February 2015\Run Stat Cell 1\BranchActivityRunStat 30-May-2016.mat';
+filesL2{5} = 'C:\Data Analysis\Tina\03 February 2015\Run Stat Cell 2\BranchActivityRunStat 17-Feb-2016.mat';
+filesL2{6} = 'C:\Data Analysis\Mary\09 April 2015\Run Stat\BranchActivityRunStat 17-Feb-2016.mat';
+filesL2{7} = 'C:\Data Analysis\Veronica\27 November 2014\Cell 2 Run Stat Gratings\BranchActivityRunStat 17-Feb-2016.mat';
+filesL2{8} = 'C:\Data Analysis\Robinson\26 June 2015\Cell 2 Run Stat\BranchActivityRunStat 11-May-2016.mat';
+filesL2{9} = 'C:\Data Analysis\Karina\09 February 2016\Cell 5 Run Stat\BranchActivityRunStat 17-Feb-2016.mat';
+filesL2{10} = 'C:\Data Analysis\Clyde\15 March 2016\Cell 1\BranchActivityRunStat 28-Apr-2016.mat';
+filesL2{11} = 'C:\Data Analysis\Clyde\15 March 2016\Cell 2\BranchActivityRunStat 28-Apr-2016.mat';
+filesL2{12} = 'C:\Data Analysis\Bonnie\14 March 2016\BranchActivityRunStat 29-Apr-2016.mat';
+filesL2{13} = 'C:\Data Analysis\Bonnie\09 February 2016\Cell 6 Gratings\BranchActivityRunStat 30-May-2016.mat';
 
-% % L5 neurons, gratings
-% filesL5{1} = 'C:\Data Analysis\Veronica\27 November 2014\MeanBranch Act RunStat 27Feb GratingsOnly\BranchActivityRunStat 27-Feb-2015.mat';
-% filesL5{2} = 'C:\Data Analysis\Gandalf\10 December 2014\Running Vs Stationary\BranchAverageActivity RunningVsStat 20Apr2015\BranchActivityRunStat 20-Apr-2015.mat';
-% filesL5{3} = 'C:\Data Analysis\Theodora\01 June 2015 Region 1\RunningActVsStat 22Aug2015\BranchActivityRunStat 22-Aug-2015.mat';
-% filesL5{4} = 'C:\Data Analysis\Theodora\01 June 2015 Region 1\Cell 2 RunningActVsStat\BranchActivityRunStat 01-Oct-2015.mat';
-% filesL5{5} = 'C:\Data Analysis\Theodora\03 June 2015 Region 1 and 2\Region 2\Cell 1\Running Vs Stat activity\BranchActivityRunStat 01-Oct-2015.mat';
-% filesL5{6} = 'C:\Data Analysis\Theodora\03 June 2015 Region 1 and 2\Region 2\Cell 2\Running Vs Stat activity\BranchActivityRunStat 01-Oct-2015.mat';
-% filesL5{7} = 'C:\Data Analysis\Theodora\03 June 2015 Region 1 and 2\Region 2\Cell 3\Running Vs Stat activity\BranchActivityRunStat 01-Oct-2015.mat';
-% filesL5{8} = 'C:\Data Analysis\Theodora\03 June 2015 Region 1 and 2\Region 2\Cell 4\Running Vs Stat activity\BranchActivityRunStat 01-Oct-2015.mat';
-% filesL5{9} = 'C:\Data Analysis\Tita\07 August 2015\Running Vs Stat activity\BranchActivityRunStat 01-Oct-2015.mat';
-% filesL5{10} = 'C:\Data Analysis\Robinson\18 June 2015\Cell 1 RunStat\BranchActivityRunStat 11-May-2016.mat';
-% filesL5{11} = 'C:\Data Analysis\Robinson\18 June 2015\Cell 2 RunStat\BranchActivityRunStat 11-May-2016.mat';
-% %filesL5{12} = 'C:\Data Analysis\Robinson\26 June 2015\Cell 1 Run Stat\BranchActivityRunStat 11-May-2016.mat';
-% filesL5{12} = 'C:\Data Analysis\Tito\12 August 2015\RunStat\BranchActivityRunStat 11-May-2016.mat';
+% L5 neurons, gratings
+filesL5{1} = 'C:\Data Analysis\Veronica\27 November 2014\MeanBranch Act RunStat 27Feb GratingsOnly\BranchActivityRunStat 27-Feb-2015.mat';
+filesL5{2} = 'C:\Data Analysis\Gandalf\10 December 2014\Running Vs Stationary\BranchAverageActivity RunningVsStat 20Apr2015\BranchActivityRunStat 20-Apr-2015.mat';
+filesL5{3} = 'C:\Data Analysis\Theodora\01 June 2015 Region 1\RunningActVsStat 22Aug2015\BranchActivityRunStat 22-Aug-2015.mat';
+filesL5{4} = 'C:\Data Analysis\Theodora\01 June 2015 Region 1\Cell 2 RunningActVsStat\BranchActivityRunStat 01-Oct-2015.mat';
+filesL5{5} = 'C:\Data Analysis\Theodora\03 June 2015 Region 1 and 2\Region 2\Cell 1\Running Vs Stat activity\BranchActivityRunStat 01-Oct-2015.mat';
+filesL5{6} = 'C:\Data Analysis\Theodora\03 June 2015 Region 1 and 2\Region 2\Cell 2\Running Vs Stat activity\BranchActivityRunStat 01-Oct-2015.mat';
+filesL5{7} = 'C:\Data Analysis\Theodora\03 June 2015 Region 1 and 2\Region 2\Cell 3\Running Vs Stat activity\BranchActivityRunStat 01-Oct-2015.mat';
+filesL5{8} = 'C:\Data Analysis\Theodora\03 June 2015 Region 1 and 2\Region 2\Cell 4\Running Vs Stat activity\BranchActivityRunStat 01-Oct-2015.mat';
+filesL5{9} = 'C:\Data Analysis\Tita\07 August 2015\Running Vs Stat activity\BranchActivityRunStat 01-Oct-2015.mat';
+filesL5{10} = 'C:\Data Analysis\Robinson\18 June 2015\Cell 1 RunStat\BranchActivityRunStat 11-May-2016.mat';
+filesL5{11} = 'C:\Data Analysis\Robinson\18 June 2015\Cell 2 RunStat\BranchActivityRunStat 11-May-2016.mat';
+filesL5{12} = 'C:\Data Analysis\Robinson\26 June 2015\Cell 1 Run Stat\BranchActivityRunStat 11-May-2016.mat';
+filesL5{13} = 'C:\Data Analysis\Tito\12 August 2015\RunStat\BranchActivityRunStat 11-May-2016.mat';
 
-% L2 neurons, spontaneous activity in the dark
-filesL2{1} = 'C:\Data Analysis\Bonnie\09 February 2016\Cell 1 Dark\BranchActivityRunStat 27-Apr-2016.mat';
-filesL2{2} = 'C:\Data Analysis\Bonnie\09 February 2016\Cell 2 Dark\BranchActivityRunStat 27-Apr-2016.mat';
-filesL2{3} = 'C:\Data Analysis\Bonnie\09 February 2016\Cell 5 Dark\BranchActivityRunStat 27-Apr-2016.mat';
-%filesL2{3} = 'C:\Data Analysis\Tina\03 February 2015\Cell 1 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
-%filesL2{4} = 'C:\Data Analysis\Tina\03 February 2015\Cell 2 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
-filesL2{4} = 'C:\Data Analysis\Mary\15 April 2015\Run Stat Dark\BranchActivityRunStat 27-Apr-2016.mat';
-%filesL2{6} = 'C:\Data Analysis\Veronica\20 November 2014\Cell 2 Run Stat Dark\BranchActivityRunStat 17-Feb-2016.mat';
-filesL2{5} = 'C:\Data Analysis\Robinson\24 June 2015\Cell 2 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
-filesL2{6} = 'C:\Data Analysis\Karina\15 January 2016\Cell 5\BranchActivityRunStat 27-Apr-2016.mat';
-
-% L5 neurons, spontaneous activity in the dark
-filesL5{1} = 'C:\Data Analysis\Veronica\20 November 2014\Cell 4 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
-filesL5{2} = 'C:\Data Analysis\Gandalf\05 December 2014\Average Activity Run Stat 17Apr2015 dark\BranchActivityRunStat 17-Apr-2015.mat';
-filesL5{3} = 'C:\Data Analysis\Theodora\28 May 2015\Cell 1 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
-filesL5{4} = 'C:\Data Analysis\Theodora\28 May 2015\Cell 2 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
-filesL5{5} = 'C:\Data Analysis\Theodora\04 June 2015\Cell 1 RunStatDark\BranchActivityRunStat 27-Apr-2016.mat';
-filesL5{6} = 'C:\Data Analysis\Theodora\04 June 2015\Cell 2 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
-filesL5{7} = 'C:\Data Analysis\Theodora\04 June 2015\Cell 3 RunStatDark\BranchActivityRunStat 27-Apr-2016.mat';
-filesL5{8} = 'C:\Data Analysis\Theodora\04 June 2015\Cell 4 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
-filesL5{9} = 'C:\Data Analysis\Tita\07 August 2015\Run Stat Dark\BranchActivityRunStat 27-Apr-2016.mat';
-
+% % L2 neurons, spontaneous activity in the dark
+% filesL2{1} = 'C:\Data Analysis\Bonnie\09 February 2016\Cell 1 Dark\BranchActivityRunStat 27-Apr-2016.mat';
+% filesL2{2} = 'C:\Data Analysis\Bonnie\09 February 2016\Cell 2 Dark\BranchActivityRunStat 27-Apr-2016.mat';
+% filesL2{3} = 'C:\Data Analysis\Bonnie\09 February 2016\Cell 5 Dark\BranchActivityRunStat 27-Apr-2016.mat';
+% %filesL2{3} = 'C:\Data Analysis\Tina\03 February 2015\Cell 1 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
+% %filesL2{4} = 'C:\Data Analysis\Tina\03 February 2015\Cell 2 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
+% filesL2{4} = 'C:\Data Analysis\Mary\15 April 2015\Run Stat Dark\BranchActivityRunStat 27-Apr-2016.mat';
+% %filesL2{6} = 'C:\Data Analysis\Veronica\20 November 2014\Cell 2 Run Stat Dark\BranchActivityRunStat 17-Feb-2016.mat';
+% filesL2{5} = 'C:\Data Analysis\Robinson\24 June 2015\Cell 2 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
+% filesL2{6} = 'C:\Data Analysis\Karina\15 January 2016\Cell 5\BranchActivityRunStat 27-Apr-2016.mat';
 % 
-% rig3 data, L5 somas and dendrites, spontaneous activity
-filesL5S{1} = 'C:\Data Analysis\Rig3\2016-March-01\Cell 2\BranchActivityRunStat 11-May-2016.mat';
-filesL5S{2} = 'C:\Data Analysis\Rig3\2016-March-02\BranchActivityRunStat 11-May-2016.mat';
-filesL5S{3} = 'C:\Data Analysis\Rig3\2016-March-11 Bonnie\Cell 1\BranchActivityRunStat 11-May-2016.mat';
-filesL5S{4} = 'C:\Data Analysis\Rig3\2016-March-11 Bonnie\Cell 2\BranchActivityRunStat 11-May-2016.mat';
-filesL5S{5} = 'C:\Data Analysis\Rig3\2016-March-11 Clyde\Cell 1\BranchActivityRunStat 11-May-2016.mat';
-filesL5S{6} = 'C:\Data Analysis\Rig3\2016-March-11 Clyde\Cell 2\BranchActivityRunStat 11-May-2016.mat';
-filesL5S{7} = 'C:\Data Analysis\Rig3\2016-March-01\Cell 1\BranchActivityRunStat 11-May-2016.mat';
+% % L5 neurons, spontaneous activity in the dark
+% filesL5{1} = 'C:\Data Analysis\Veronica\20 November 2014\Cell 4 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
+% filesL5{2} = 'C:\Data Analysis\Gandalf\05 December 2014\Average Activity Run Stat 17Apr2015 dark\BranchActivityRunStat 17-Apr-2015.mat';
+% filesL5{3} = 'C:\Data Analysis\Theodora\28 May 2015\Cell 1 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
+% filesL5{4} = 'C:\Data Analysis\Theodora\28 May 2015\Cell 2 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
+% filesL5{5} = 'C:\Data Analysis\Theodora\04 June 2015\Cell 1 RunStatDark\BranchActivityRunStat 27-Apr-2016.mat';
+% filesL5{6} = 'C:\Data Analysis\Theodora\04 June 2015\Cell 2 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
+% filesL5{7} = 'C:\Data Analysis\Theodora\04 June 2015\Cell 3 RunStatDark\BranchActivityRunStat 27-Apr-2016.mat';
+% filesL5{8} = 'C:\Data Analysis\Theodora\04 June 2015\Cell 4 RunStat Dark\BranchActivityRunStat 27-Apr-2016.mat';
+% filesL5{9} = 'C:\Data Analysis\Tita\07 August 2015\Run Stat Dark\BranchActivityRunStat 27-Apr-2016.mat';
+% 
+% % 
+% % rig3 data, L5 somas and dendrites, spontaneous activity
+% filesL5S{1} = 'C:\Data Analysis\Rig3\2016-March-01\Cell 2\BranchActivityRunStat 11-May-2016.mat';
+% filesL5S{2} = 'C:\Data Analysis\Rig3\2016-March-02\BranchActivityRunStat 11-May-2016.mat';
+% filesL5S{3} = 'C:\Data Analysis\Rig3\2016-March-11 Bonnie\Cell 1\BranchActivityRunStat 11-May-2016.mat';
+% filesL5S{4} = 'C:\Data Analysis\Rig3\2016-March-11 Bonnie\Cell 2\BranchActivityRunStat 11-May-2016.mat';
+% filesL5S{5} = 'C:\Data Analysis\Rig3\2016-March-11 Clyde\Cell 1\BranchActivityRunStat 11-May-2016.mat';
+% filesL5S{6} = 'C:\Data Analysis\Rig3\2016-March-11 Clyde\Cell 2\BranchActivityRunStat 11-May-2016.mat';
+% filesL5S{7} = 'C:\Data Analysis\Rig3\2016-March-01\Cell 1\BranchActivityRunStat 11-May-2016.mat';
 
 
 % initialise
@@ -95,20 +96,20 @@ for f = 1:length(filesL5)
 end
 
 
-% add rig3 data?
-Soma5Stat = [];
-Soma5Run = [];
-for f = 1:length(filesL5S)
-    
-    % L5 neurons
-    load(filesL5S{f},'ActivitySegStat','ActivitySegRun')
-    
-    BranchesStat5 = [BranchesStat5 ActivitySegStat(3:end)];
-    BranchesRun5 = [BranchesRun5 ActivitySegRun(3:end)];
-    
-    Soma5Stat = [Soma5Stat ActivitySegStat(1)];
-    Soma5Run = [Soma5Run ActivitySegRun(1)];
-end
+% % add rig3 data?
+% Soma5Stat = [];
+% Soma5Run = [];
+% for f = 1:length(filesL5S)
+%     
+%     % L5 neurons
+%     load(filesL5S{f},'ActivitySegStat','ActivitySegRun')
+%     
+%     BranchesStat5 = [BranchesStat5 ActivitySegStat(3:end)];
+%     BranchesRun5 = [BranchesRun5 ActivitySegRun(3:end)];
+%     
+%     Soma5Stat = [Soma5Stat ActivitySegStat(1)];
+%     Soma5Run = [Soma5Run ActivitySegRun(1)];
+% end
 
 
 % compute mean and standard error of the mean
@@ -127,10 +128,10 @@ SomaRunMean = nanmean(SomaRun);
 SomaStatSem = nanstd(SomaStat)/sqrt(length(SomaStat));
 SomaRunSem = nanstd(SomaRun)/sqrt(length(SomaRun));
 
-Soma5StatMean = nanmean(Soma5Stat);
-Soma5RunMean = nanmean(Soma5Run);
-Soma5StatSem = nanstd(Soma5Stat)/sqrt(length(Soma5Stat));
-Soma5RunSem = nanstd(Soma5Run)/sqrt(length(Soma5Run));
+% Soma5StatMean = nanmean(Soma5Stat);
+% Soma5RunMean = nanmean(Soma5Run);
+% Soma5StatSem = nanstd(Soma5Stat)/sqrt(length(Soma5Stat));
+% Soma5RunSem = nanstd(Soma5Run)/sqrt(length(Soma5Run));
 
 % paired t-test
 [h2, p2] = ttest(BranchesStat2, BranchesRun2);
@@ -142,8 +143,8 @@ disp(['Paired t-test for L5 branches: ' num2str(h5) ' p-value = ' num2str(p5)])
 [h2S, p2S] = ttest(SomaStat, SomaRun);
 disp(['Paired t-test for L2 somas: ' num2str(h2S) ' p-value = ' num2str(p2S)])
 
-[h5S, p5S] = ttest(Soma5Stat, Soma5Run);
-disp(['Paired t-test for L5 somas: ' num2str(h5S) ' p-value = ' num2str(p5S)])
+% [h5S, p5S] = ttest(Soma5Stat, Soma5Run);
+% disp(['Paired t-test for L5 somas: ' num2str(h5S) ' p-value = ' num2str(p5S)])
 
 % plot
 figure;
@@ -170,10 +171,38 @@ xlim([0 3])
 title('Dendrites of L5 neurons')
 box off
 
+% figure;
+% bar([1 2],[Soma5StatMean Soma5RunMean])
+% hold on
+% errorbar([1 2],[Soma5StatMean Soma5RunMean],[Soma5StatSem Soma5RunSem],'.k')
+% xlim([0 3])
+% title('Somas of L5 neurons')
+% box off
+
+% plot all datapoints
 figure;
-bar([1 2],[Soma5StatMean Soma5RunMean])
-hold on
-errorbar([1 2],[Soma5StatMean Soma5RunMean],[Soma5StatSem Soma5RunSem],'.k')
+for br = 1:length(BranchesStat2)
+    plot([1 2],[BranchesStat2(br) BranchesRun2(br)],'-ko')
+    hold all
+end
 xlim([0 3])
-title('Somas of L5 neurons')
 box off
+title('Dendrites of L2 neurons')
+
+figure;
+for br = 1:length(SomaStat)
+    plot([1 2],[SomaStat(br) SomaRun(br)],'-ko')
+    hold all
+end
+xlim([0 3])
+box off
+title('Somas of L2 neurons')
+
+figure;
+for br = 1:length(BranchesRun5)
+    plot([1 2],[BranchesStat5(br) BranchesRun5(br)],'-ko')
+    hold all
+end
+xlim([0 3])
+box off
+title('Dendrites of L5 neurons')
